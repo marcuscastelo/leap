d05522fd572bd9bfa910275718c99ffe0f015a48
marcuscastelo <marcuscastelo@usp.br>
2024-04-08 19:41:27 -0300

    LP Tests

--- contracts/LeapETHPool.sol
+++ contracts/LeapETHPool.sol
@@ -5,39 +5,65 @@ import "./LeapCoin.sol";
 
 contract LeapETHPool {
     LeapCoin public leapContract_;
+    bool public initialized_;
 
     event Initialized(address indexed initiator, uint256 leapAmount, uint256 ethAmount);
-    event Swapped(address indexed initiator, uint256 leapAmount, uint256 ethAmount);
+    event Swap(address indexed initiator, uint256 leapAmount, uint256 ethAmount);
     event Deposited(address indexed initiator, uint256 leapAmount, uint256 ethAmount);
     event Withdrawn(address indexed initiator, uint256 leapAmount, uint256 ethAmount);
 
     constructor (address _leapContract) {
         leapContract_ = LeapCoin(_leapContract);
+        initialized_ = false;
+        // Send all initial ETH to 0x0
+        payable(0x0).transfer(address(this).balance);
     }
 
-    modifier poolEmpty() {
-        require(leapContract_.balanceOf(address(this)) == 0, "Pool not empty (LEAP)");
-        require(address(this).balance == 0, "Pool not empty (ETH)");
+    modifier onlyInitialized() {
+        require(initialized_, "LeapETHPool: Pool not initialized");
         _;
     }
 
-    function leapReserve() external view returns (uint256) {
+    modifier onlyNotInitialized() {
+        require(!initialized_, "LeapETHPool: Pool already initialized");
+        _;
+    }
+
+    function leapReserve() public view returns (uint256) {
         return leapContract_.balanceOf(address(this));
     }
 
-    function ethReserve() external view returns (uint256) {
+    function ethReserve() public view returns (uint256) {
         return address(this).balance;
     }
 
-    function initializePool(uint256 _leapAmount) payable poolEmpty external {
-        require(_leapAmount > 0, "Invalid LEAP amount");
-        require(msg.value > 0, "Invalid ETH amount");
-        require(leapContract_.allowance(msg.sender, address(this)) >= _leapAmount, "Insufficient allowance");
+    function initializePool(uint256 _leapAmount) onlyNotInitialized payable external {
+        require(_leapAmount > 0, "LeapETHPool: Invalid LEAP amount");
+        require(msg.value > 0, "LeapETHPool: Invalid ETH amount");
+        require(leapReserve() == 0, "LeapETHPool: Pool already initialized (LEAP)");
+        require(ethReserve() == msg.value, "LeapETHPool: Pool already initialized (ETH)");
 
-        require(leapContract_.transferFrom(msg.sender, address(this), _leapAmount), "LEAP transfer failed");
+        require(leapContract_.allowance(msg.sender, address(this)) >= _leapAmount, "LeapETHPool: Insufficient allowance");
+        require(leapContract_.transferFrom(msg.sender, address(this), _leapAmount), "LeapETHPool: LEAP transfer failed");
+
+        initialized_ = true;
     }
 
-    function quote(uint256 leapAmount) external view returns (uint256 ethAmount) {
-        return leapAmount;
+    function quote(uint256 leapAmount) onlyInitialized public view returns (uint256 ethAmount) {
+        return leapAmount * (ethReserve() / leapReserve());
+    }
+
+    function swapToEth(uint256 leapAmount) onlyInitialized external {
+        require(leapAmount > 0, "LeapETHPool: Invalid LEAP amount");
+        require(leapContract_.allowance(msg.sender, address(this)) >= leapAmount, "LeapETHPool: Insufficient allowance");
+
+        uint256 ethAmount = quote(leapAmount);
+        require(ethAmount <= ethReserve(), "LeapETHPool: Insufficient ETH reserve on the pool");
+
+        require(leapContract_.transferFrom(msg.sender, address(this), leapAmount), "LeapETHPool: LEAP transfer failed");
+        require(leapAmount != 666, "LeapETHPool: Breakpoint force revert"); // TODO: tirar isso em produção
+        payable(msg.sender).transfer(ethAmount);
+
+        emit Swap(msg.sender, leapAmount, ethAmount);
     }
 }
--- test/LeapETHPool.ts
+++ test/LeapETHPool.ts
@@ -30,8 +30,6 @@ describe("LeapETHPool", async () => {
         // Deploy LeapETHPool
         const poolFactory = await ethers.getContractFactory("LeapETHPool");
         pool = await poolFactory.deploy(await token.getAddress());
-
-        pool.initializePool(1);
     });
 
     afterEach(async () => {
@@ -61,7 +59,130 @@ describe("LeapETHPool", async () => {
         expect(balance).to.equal(0, "LEAP balance is incorrect");
     });
 
-    it("Should have correct amounts of ETH and LEAP in the pool after init", async () => {
-        pool.initializePool(1);
+    it("Should error if initialized with 0 ETH", async () => {
+        await expect(pool.initializePool(1, { value: ethers.parseEther("0") })).
+        to.be.revertedWith("LeapETHPool: Invalid ETH amount");
     });
+
+    it("Should error if initialized with 0 LEAP", async () => {
+        await expect(pool.initializePool(0, { value: ethers.parseEther("1") })).
+        to.be.revertedWith("LeapETHPool: Invalid LEAP amount");
+    });
+
+    it("Should error if initialized with 0 LEAP allowance", async () => {
+        await expect(pool.initializePool(1, { value: ethers.parseEther("1") })).
+        to.be.revertedWith("LeapETHPool: Insufficient allowance");
+    });
+
+    it("Should error if initializedw with more than allowance", async () => {
+        await token.approve(await pool.getAddress(), ethers.parseUnits("1", decimals));
+        await expect(pool.initializePool(ethers.parseUnits("1000", decimals), { value: ethers.parseEther("1") })).
+        to.be.revertedWith("LeapETHPool: Insufficient allowance");
+    });
+
+    it("Should have correct amounts of ETH and LEAP, and quote in the pool after init", async () => {
+        await token.approve(await pool.getAddress(), ethers.parseUnits("1", decimals));
+        await pool.initializePool(ethers.parseUnits("1", decimals), { value: ethers.parseEther("1") });
+        const address = await pool.getAddress();
+        const ethBalance = await ethers.provider.getBalance(address);
+        const tokenBalance = await token.balanceOf(address);
+        expect(ethBalance).to.equal(ethers.parseEther("1"), "ETH balance is incorrect");
+        expect(tokenBalance).to.equal(ethers.parseUnits("1", decimals), "ETH balance is incorrect");
+
+        const quote = await pool.quote(ethers.parseUnits("1", decimals));
+        expect(quote).to.equal(ethers.parseEther("1"), "Quote is incorrect");
+    });
+
+    it("Should have correct quote with 100 to 1 in the pool after init", async () => {
+        await token.approve(await pool.getAddress(), ethers.parseUnits("1", decimals));
+        await pool.initializePool(ethers.parseUnits("1", decimals), { value: ethers.parseEther("100") });
+        const address = await pool.getAddress();
+        const ethBalance = await ethers.provider.getBalance(address);
+        const tokenBalance = await token.balanceOf(address);
+        expect(ethBalance).to.equal(ethers.parseEther("100"), "ETH balance is incorrect");
+        expect(tokenBalance).to.equal(ethers.parseUnits("1", decimals), "ETH balance is incorrect");
+
+        const quote = await pool.quote(ethers.parseUnits("1", decimals));
+        expect(quote).to.equal(ethers.parseEther("100"), "Quote is incorrect");
+    });
+
+    it("Should error if initialized twice", async () => {
+        await token.approve(await pool.getAddress(), ethers.parseUnits("1", decimals));
+        await pool.initializePool(ethers.parseUnits("1", decimals), { value: ethers.parseEther("1") });
+        await token.approve(await pool.getAddress(), ethers.parseUnits("1", decimals));
+        await expect(pool.initializePool(ethers.parseUnits("1", decimals), { value: ethers.parseEther("1") })).
+        to.be.revertedWith("LeapETHPool: Pool already initialized");
+    });
+
+    it("Should error if swap on uninitialized pool", async () => {
+        await expect(pool.swapToEth(ethers.parseUnits("1", decimals))).to.revertedWith("LeapETHPool: Pool not initialized");
+    });
+
+    it("Should error if swap with 0 LEAP allowance", async () => {
+        await token.approve(await pool.getAddress(), ethers.parseUnits("1", decimals));
+        await pool.initializePool(ethers.parseUnits("1", decimals), { value: ethers.parseEther("1") });
+        await expect(pool.swapToEth(ethers.parseUnits("1", decimals))).to.revertedWith("LeapETHPool: Insufficient allowance");
+    });
+
+    it("Should swap 1 LEAP correctly", async () => {
+        await token.approve(await pool.getAddress(), ethers.parseUnits("1", decimals));
+        await pool.initializePool(ethers.parseUnits("1", decimals), { value: ethers.parseEther("1") });
+
+        const leapBalanceBefore = await token.balanceOf(await deployer.getAddress());
+        const ethBalanceBefore = await ethers.provider.getBalance(await deployer.getAddress());
+
+        const approveResponse = await token.approve(await pool.getAddress(), ethers.parseUnits("1", decimals));
+
+        const quote = await pool.quote(ethers.parseUnits("1", decimals));
+        await expect(pool.swapToEth(ethers.parseUnits("1", decimals))).to.emit(pool, "Swap").withArgs(deployer, ethers.parseUnits("1", decimals), ethers.parseEther("1"));
+
+        const leapBalanceAfter = await token.balanceOf(await deployer.getAddress());
+        const ethBalanceAfter = await ethers.provider.getBalance(await deployer.getAddress());
+
+        expect(leapBalanceAfter).to.equal(leapBalanceBefore - ethers.parseUnits("1", decimals), "LEAP balance is incorrect");
+        // TODO: get spent gas and include in assertion
+        expect(ethBalanceAfter).to.greaterThan(ethBalanceBefore + ethers.parseEther("0.001"), "ETH balance is too low");
+        expect(ethBalanceAfter).to.lessThan(ethBalanceBefore + quote, "ETH balance is too big");
+
+        expect(await pool.ethReserve()).to.equal(ethers.parseEther("0"), "ETH reserve is incorrect");
+        expect(await pool.leapReserve()).to.equal(ethers.parseUnits("2", decimals), "LEAP reserve is incorrect");
+        expect(await pool.quote(ethers.parseUnits("1", decimals))).to.equal(ethers.parseEther("0"), "Quote is incorrect");
+    });
+
+    it("Should swap 0.5 LEAP correctly", async () => {
+        await token.approve(await pool.getAddress(), ethers.parseUnits("1", decimals));
+        await pool.initializePool(ethers.parseUnits("1", decimals), { value: ethers.parseEther("1") });
+
+        const leapBalanceBefore = await token.balanceOf(await deployer.getAddress());
+        const ethBalanceBefore = await ethers.provider.getBalance(await deployer.getAddress());
+
+        const approveResponse = await token.approve(await pool.getAddress(), ethers.parseUnits("0.5", decimals));
+
+        const quote = await pool.quote(ethers.parseUnits("0.5", decimals));
+        console.log("Quote: ", quote.toString());
+        await expect(pool.swapToEth(ethers.parseUnits("0.5", decimals))).to.emit(pool, "Swap").withArgs(deployer, ethers.parseUnits("0.5", decimals), ethers.parseEther("0.5"));
+
+        const leapBalanceAfter = await token.balanceOf(await deployer.getAddress());
+        const ethBalanceAfter = await ethers.provider.getBalance(await deployer.getAddress());
+
+        expect(leapBalanceAfter).to.equal(leapBalanceBefore - ethers.parseUnits("0.5", decimals), "LEAP balance is incorrect");
+        // TODO: get spent gas and include in assertion
+        expect(ethBalanceAfter).to.greaterThan(ethBalanceBefore + ethers.parseEther("0.001"), "ETH balance is too low");
+        expect(ethBalanceAfter).to.lessThan(ethBalanceBefore + quote, "ETH balance is too big");
+
+        expect(await pool.ethReserve()).to.equal(ethers.parseEther("0.5"), "ETH reserve is incorrect");
+        expect(await pool.leapReserve()).to.equal(ethers.parseUnits("1.5", decimals), "LEAP reserve is incorrect");
+        const newQuote = await pool.quote(ethers.parseUnits("0.5", decimals));
+        expect(newQuote).to.equal(ethers.toBigInt(5e8) * ethers.toBigInt(1e9), "Quote is incorrect");
+
+        console.log("New quote: ", );
+    });
+
+    it("Should error if swapping more than 1 LEAP", async () => {
+        await token.approve(await pool.getAddress(), ethers.parseUnits("1", decimals));
+        await pool.initializePool(ethers.parseUnits("1", decimals), { value: ethers.parseEther("1") });
+
+        await token.approve(await pool.getAddress(), ethers.parseUnits("2", decimals));
+        await expect(pool.swapToEth(ethers.parseUnits("2", decimals))).to.revertedWith("LeapETHPool: Insufficient ETH reserve on the pool");
+    })
 });
