3e194299b750141dbf0f30a7981a31736f3f3f8c
marcuscastelo <marcuscastelo@usp.br>
2024-07-16 14:15:06 -0300

    clean: remove old login page

--- frontend/src/app/login/LoginFormInput.tsx
+++
@@ -1,9 +0,0 @@
-import { InputHTMLAttributes } from 'react'
-import { cn } from '~/lib/utils'
-
-export function LoginFormInput(props: InputHTMLAttributes<HTMLInputElement>) {
-  const className =
-    'text-center bg-slate-700 border border-slate-900 rounded p-2 focus:placeholder-transparent'
-
-  return <input {...props} className={cn(className, props.className)}></input>
-}
